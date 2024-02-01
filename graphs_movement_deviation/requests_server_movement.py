53c59443c20d7855e9f26b7cc85e1b942a71f1ae
Edems10 <edemsikk@gmail.com>
2024-02-01 17:38:52 +0100

    update_structure

---
+++ graphs_movement_deviation/requests_server_movement.py
@@ -0,0 +1,42 @@
+import requests
+import os
+import json
+
+# api-endpoint
+BASE_URL = "http://90.180.16.91:1337/edems_swag/legue_predict/1.0.0/predictionJson"
+
+# Directory to store the prediction files
+output_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'games_movement_prediction')
+
+# Create the directory if it doesn't exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'games.txt')
+
+print(file_path)
+
+with open(file_path, 'r') as file:
+    for line in file:
+        game = line.strip()  # Remove any extra whitespace or newline characters
+        game_json = game + '.json'
+        url = f"{BASE_URL}?game={game_json}&scale=1000&scale=1000"
+        print(url)
+        r = requests.get(url)
+
+        game_path = os.path.join(output_directory, game_json)
+        try:
+            data = r.json()
+            with open(game_path, 'w') as file_game:
+                json.dump(data, file_game, indent=4)
+        except json.JSONDecodeError as e:
+            # Handle the JSON decoding error here
+            data = None  # Assign None or handle the situation accordingly
+            print("JSONDecodeError with:", game)
+            with open(game_path, 'w') as file_game:
+                json.dump(data, file_game, indent=4)
+        except requests.RequestException as e:
+            # Handle the request exception here
+            print("RequestException with:", game)
+            data = None  # Assign None or handle the situation accordingly
+            json.dump(data, file_game, indent=4)
